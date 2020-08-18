7b6425827b19a2f8cdb803547661de341003de49
aaron276h <aaron@determined.ai>
2020-08-18 12:49:04 -0400

    chore: address review feedback

--- master/internal/kubernetes/pods.go
+++ master/internal/kubernetes/pods.go
@@ -2,17 +2,6 @@
 // and stopping tasks, monitoring their status, and fetching logs.
 package kubernetes
 
-// High lever overview of the actors within the kubernetes package:
-//
-//   pods
-//     +- pod(s): manages pod lifecycle. One per container in a task.
-//        +- podLogStreamer: stream logs for a specific pod.
-//     +- informer: sends updates about pod states
-//     +- events: sends updates about kubernetes events.
-//     +- tokens: provisions tokens for kubernetes resource creation.
-//     +- kubernetesDeletionDealer: round robins kubernetes deletion requests
-//        +- kubernetesDeletionWorker(s): processes deletion requests.
-
 import (
 	"fmt"
 	"net/http"
@@ -44,6 +33,17 @@ type podMetadata struct {
 	containerID string
 	latestPod   *k8sV1.Pod
 }
+
+// High lever overview of the actors within the kubernetes package:
+//
+//   pods
+//     +- pod(s): manages pod lifecycle. One per container in a task.
+//        +- podLogStreamer: stream logs for a specific pod.
+//     +- informer: sends updates about pod states
+//     +- events: sends updates about kubernetes events.
+//     +- tokens: provisions tokens for kubernetes resource creation.
+//     +- kubernetesDeletionDealer: round robins kubernetes deletion requests
+//        +- kubernetesDeletionWorker(s): processes deletion requests.
 
 type pods struct {
 	cluster                  *actor.Ref
